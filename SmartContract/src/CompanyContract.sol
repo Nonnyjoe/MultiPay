dd567bd3a334701aa14ec347895e23bd0729101f
Ginika Chinonso <ginika.chinonso.francis@gmail.com>
2023-09-07 17:54:51 +0100

    feat: Factory update

--- SmartContract/src/CompanyContract.sol
+++ SmartContract/src/CompanyContract.sol
@@ -1,11 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract ChildCon {
+contract CompanyContract {
 
+    string public name;
+    string public symbol;
+    address public admin;
+    uint256 public nftId;
     uint256 public totalSubAvailiable;
     PlansDetails[] public availiablePlans;
-    mapping (uint256 => PlanDetails) public IdToPlanDetails;
+    mapping (uint256 => PlansDetails) public IdToPlanDetails;
 
     struct PlansDetails {
         string planName;
@@ -16,11 +20,21 @@ contract ChildCon {
 
     struct UserDetails {
         address userAddress;
-        
+        string userEmail;
         uint256 timeOfSubscription;
         uint256 subscriptionEnds;
         bool autoSubscribe;
     }
+
+
+    constructor(string memory _name, string memory _symbol, uint256 _nftId){
+        name = _name;
+        symbol = _symbol;
+        nftId = _nftId;
+        admin = msg.sender;
+
+    }
+
 
     function createPlan(uint256 newNumber) public {
         //
@@ -31,9 +45,6 @@ contract ChildCon {
     }
 
     function autoRenew() public {
-        number++;
+        // number++;
     }
 }
-
-
-
--- SmartContract/src/FactoryContract.sol
+++ SmartContract/src/FactoryContract.sol
@@ -1,9 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
+import "./CompanyContract.sol";
+
 contract FactoryCon {
+
+    address public admin;
+    uint256 public totalCompanies = 0;
+    mapping (uint256 => CompanyDetails) public IdToCompanyDetails;
+    CompanyDetails[] public allCompanies;
+
+    //
+
+
     uint256 public totalCompaniesID;
-    mapping (uint256 companyId => CompanyDetails) public IdToCompanyDetails;
     address[] companyAddresses;
     mapping (address owner => address companyAdd) public ownerToCompany;
     mapping (address companyAddress => uint256 companyID) public Identity;
@@ -11,13 +21,30 @@ contract FactoryCon {
 
     struct CompanyDetails {
         string CompanyName;
-        address CompanyAddress;
+        string CompanySymbol;
         address adminAddress;
+        address CompanyAddress;
     }
 
-    
 
-    function autoRenew() public {
-        //
+
+    constructor( ) {
+        admin = msg.sender;
+    }
+
+
+    function createCompany(string memory _name, string memory _symbol) public returns (CompanyDetails memory) {
+
+        CompanyContract _companyCon = new CompanyContract(_name, _symbol, totalCompanies);
+
+        CompanyDetails storage _company = allCompanies[totalCompanies];
+        _company.CompanyName = _name;
+        _company.CompanySymbol = _symbol;
+        _company.adminAddress = msg.sender;
+        _company.CompanyAddress = address(_companyCon);
+
+        totalCompanies++;
+
+        return (_company);
     }
 }
