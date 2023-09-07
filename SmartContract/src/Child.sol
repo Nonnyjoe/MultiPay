94a01f96dc465205386393a598c7356b50cbd1ef
Ginika Chinonso <ginika.chinonso.francis@gmail.com>
2023-09-07 16:16:33 +0100

    feat: Contract template

---
+++ SmartContract/src/Child.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract ChildCon {
+
+    uint256 public totalSubAvailiable;
+    PlansDetails[] public availiablePlans;
+    mapping (uint256 => PlanDetails) public IdToPlanDetails;
+
+    struct PlansDetails {
+        string planName;
+        uint256 price;
+        uint256 totalSubscribers;
+        UserDetails[] subscribersData;
+    }
+
+    struct UserDetails {
+        address userAddress;
+        
+        uint256 timeOfSubscription;
+        uint256 subscriptionEnds;
+        bool autoSubscribe;
+    }
+
+    function createPlan(uint256 newNumber) public {
+        //
+    }
+
+    function subscribe(uint256 PlanId) public {
+        //
+    }
+
+    function autoRenew() public {
+        number++;
+    }
+}
+
+
+contract FactoryCon {
+    uint256 public totalCompanies;
+    mapping (uint => CompanyDetails) public IdToCompanyDetails;
+
+    struct CompanyDetails {
+        string name;
+        address admin;
+
+    }
+
+    
+
+    function autoRenew() public {
+        //
+    }
+}
