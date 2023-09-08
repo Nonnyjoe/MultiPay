c2834f9c98ef68f54c5096ffaba555789f702d98
Nonnyjoe <idogwuchinonso1@gmail.com>
2023-09-08 21:17:23 +0100

    added subscribe function

--- SmartContract/src/FactoryContract.sol
+++ SmartContract/src/FactoryContract.sol
@@ -5,15 +5,8 @@ pragma solidity ^0.8.13;
 import "./CompanyContract.sol";
 
 contract FactoryCon {
-    struct CompanyDetails {
-        string CompanyName;
-        string CompanySymbol;
-        address adminAddress;
-        address CompanyAddress;
-    }
 
-    CompanyDetails[] public allCompanies;
-    mapping (address => CompanyDetails) public addressToCompanyDetails;
+    // mapping (address => CompanyDetails) public addressToCompanyDetails;
     uint256 public totalCompaniesID;
     address[] public companyAddresses;
     address public admin;
@@ -22,31 +15,26 @@ contract FactoryCon {
     //company address => companyID
     mapping (address => uint256) public Identity;
     bool paused;
+
     constructor( ) {
         admin = msg.sender;
     }
 
-    function createCompany(string memory _name, string memory _symbol) public returns (CompanyDetails memory) {
+    function createCompany(string memory _name, string memory _symbol) public  {
         totalCompaniesID++;
-        CompanyContract _companyCon = new CompanyContract(_name, _symbol, totalCompaniesID);
-
-        CompanyDetails storage _company = allCompanies[totalCompaniesID];
-        _company.CompanyName = _name;
-        _company.CompanySymbol = _symbol;
-        _company.adminAddress = msg.sender;
-        _company.CompanyAddress = address(_companyCon);
-
-
-
-        return (_company);
+        CompanyContract _companyCon = new CompanyContract(_name, _symbol, totalCompaniesID, msg.sender);
+        companyAddresses.push(address(_companyCon));
+        ownerToCompany[msg.sender] = address(_companyCon);
+        Identity[address(_companyCon)] = totalCompaniesID;
     }
 
 
-    function GetALLAddresses() external view returns(CompanyDetails[] memory){
-        return allCompanies;
+    function GetALLAddresses() external view returns(address[] memory ){
+        return companyAddresses;
     }
-    function GetSpecificAddress(address _SmartAcctAddress) external view returns(address){
-        return ownerToCompany[_SmartAcctAddress];
+
+    function GetSpecificAddress(address _owner) external view returns(address){
+        return ownerToCompany[_owner];
     }
      function pause() external{
         require(msg.sender == admin, 'Not authorized');
