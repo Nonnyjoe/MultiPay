35aedef617536ab9de5ff896c9762023333fbd8b
WiseMrMusa <idogwuchinonso1@gmail.com>
2023-09-09 11:02:16 +0100

    designed create organization

--- packages/nextjs/components/CreateOrganization.tsx
+++ packages/nextjs/components/CreateOrganization.tsx
@@ -1,84 +1,85 @@
 import { useContext, useState } from "react";
+import FactoryAbi from "../utils/FactoryAbi.json";
+import { Button } from "./Button";
 import { Container } from "./Container";
 import { FadeIn } from "./FadeIn";
-import { ethers } from "ethers";
-import { FactoryAddr } from "~~/utils/FactoryAddress";
-import FactoryAbi from "../utils/FactoryAbi.json";
-import { SmartAccountContext } from "~~/context/SmartAccount";
 import { IHybridPaymaster, PaymasterMode, SponsorUserOperationDto } from "@biconomy/paymaster";
+import { ethers } from "ethers";
+import { SmartAccountContext } from "~~/context/SmartAccount";
+import { FactoryAddr } from "~~/utils/FactoryAddress";
 
 export const CreateOrganization = () => {
+  const [organizationName, setOrganizationName] = useState("");
+  const [organizationSymbol, setOrganizationSymbol] = useState("");
 
-    const [organizationName, setOrganizationName] = useState("");
-    const [organizationSymbol, setOrganizationSymbol] = useState("");
+  const { provider, smartAccount } = useContext(SmartAccountContext);
 
-    const {provider, smartAccount} = useContext(SmartAccountContext);
+  const contract = new ethers.Contract(FactoryAddr(), FactoryAbi, provider);
 
+  const createOrganization = async (e: any) => {
+    e.preventDefault();
+    const minTx = await contract.populateTransaction.createCompany(
+      organizationName,
+      organizationSymbol,
+      "0xe2dd3E46257cc1AD413F679Caea89A560fdE3e38",
+    );
+    console.log(minTx.data);
+    const tx1 = {
+      to: FactoryAddr(),
+      data: minTx.data,
+    };
+    const userOp = await smartAccount?.buildUserOp([tx1]);
+    console.log({ userOp });
 
-    const contract = new ethers.Contract(FactoryAddr(), FactoryAbi, provider);
+    const biconomyPaymaster = smartAccount?.paymaster as IHybridPaymaster<SponsorUserOperationDto>;
+    const paymasterServiceData: SponsorUserOperationDto = {
+      mode: PaymasterMode.SPONSORED,
+    };
+    const paymasterAndDataResponse = await biconomyPaymaster.getPaymasterAndData(userOp, paymasterServiceData);
 
+    userOp.paymasterAndData = paymasterAndDataResponse.paymasterAndData;
+    const userOpResponse = await smartAccount.sendUserOp(userOp);
+    console.log("userOpHash", userOpResponse);
+    const { receipt } = await userOpResponse.wait(1);
+    console.log("txHash", receipt.transactionHash);
+  };
 
-    const createOrganization = async (e: any) => {
-        e.preventDefault();
-        const minTx = await contract.populateTransaction.createCompany(organizationName, organizationSymbol, "0xe2dd3E46257cc1AD413F679Caea89A560fdE3e38");
-        console.log(minTx.data);
-        const tx1 = {
-            to: FactoryAddr(),
-            data: minTx.data,
-        };
-        let userOp = await smartAccount?.buildUserOp([tx1]);
-        console.log({ userOp });
-
-        const biconomyPaymaster = smartAccount?.paymaster as IHybridPaymaster<SponsorUserOperationDto>;
-        let paymasterServiceData: SponsorUserOperationDto = {
-            mode: PaymasterMode.SPONSORED,
-        };
-        const paymasterAndDataResponse = await biconomyPaymaster.getPaymasterAndData(
-            userOp,
-            paymasterServiceData
-            );
-
-        userOp.paymasterAndData = paymasterAndDataResponse.paymasterAndData;
-        const userOpResponse = await smartAccount.sendUserOp(userOp);
-        console.log("userOpHash", userOpResponse);
-        const { receipt } = await userOpResponse.wait(1);
-        console.log("txHash", receipt.transactionHash);
-    }
-
-    return(
-        <Container className="mt-24 sm:mt-32 md:mt-56">
-            <FadeIn className="max-w-3xl">
-                <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-                    Create an Organization
-                </h1>
-                <form className="flex flex-col gap-3">
-                    <label htmlFor="organization-name">
-                        Organization Name 
-                        <input 
-                            type="text" 
-                            name="organization-symbol" 
-                            id="" 
-                            className="border rounded-lg mx- 4 p-2" 
-                            onChange={(e) => {setOrganizationName(e.target.value)}}
-                        />
-                    </label>
-                    <label htmlFor="organization-symbol">
-                        Organization Symbol 
-                        <input 
-                            type="text" 
-                            name="organization-symbol" 
-                            id="" 
-                            className="border rounded-lg mx- 4 p-2" 
-                            onChange={(e) => {setOrganizationSymbol(e.target.value)}}
-                        />
-                    </label>
-                    <button 
-                        type="submit" 
-                        onClick={createOrganization}
-                        className="m-0 p-2 border rounded-lg"
-                    >Create Organization</button>
-                </form>
-            </FadeIn>
-        </Container>
-    )
-}
+  return (
+    <Container className="mt-14 sm:mt-32 md:mt-56">
+      <FadeIn className="max-w-3xl">
+        <form className="flex flex-col bg-white p-8 rounded-lg gap-6">
+          <h1 className="font-medium font-display text-5xl tracking-tight text-neutral-950 [text-wrap:balance]">
+            Create an Organization
+          </h1>
+          <div className="flex flex-col">
+            <label htmlFor="organization-name">Organization Name</label>
+            <input
+              type="text"
+              name="organization-symbol"
+              id=""
+              className="border rounded-lg mx- 4 p-2"
+              onChange={e => {
+                setOrganizationName(e.target.value);
+              }}
+            />
+          </div>
+          <div className="flex flex-col">
+            <label htmlFor="organization-symbol">Organization Symbol</label>
+            <input
+              type="text"
+              name="organization-symbol"
+              id=""
+              className="border rounded-lg mx- 4 p-2"
+              onChange={e => {
+                setOrganizationSymbol(e.target.value);
+              }}
+            />
+          </div>
+          <Button type="submit" onClick={createOrganization} className="m-0 p-2 border rounded-lg">
+            Create Organization
+          </Button>
+        </form>
+      </FadeIn>
+    </Container>
+  );
+};
--- packages/nextjs/components/SideNav.tsx
+++ packages/nextjs/components/SideNav.tsx
@@ -1,3 +1,4 @@
+import { CSSProperties } from "react";
 import Link from "next/link";
 
 export const SideNav = () => {
@@ -5,25 +6,19 @@ export const SideNav = () => {
     <nav className=" mt-10 block">
       <ul role="list">
         <li className=" mt-6 relative"></li>
-        <h2 className=" text-xs font-semibold text-zinc-900"> Routes </h2>
+        <h2 className=" font-semibold text-zinc-900"> Routes </h2>
         <div className=" relative mt-3 pl-2">
-          {/* <div
-                        className="absolute inset-x-0 top-0 h-16 rounded-md bg-zinc-800/5 will-change-transform z-50"
-                        data-projection-id="26"
-                    // style="height: 32px; top: 0px; border-radius: 2.95203% / 25%; opacity: 1; transform: none; transform-origin: 50% 50% 0px;"
-                    ></div> */}
-
           {/* LINE */}
           <div
-            className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 "
-            // style="transform: none; transform-origin: 50% 50% 0px;"
+            className="absolute inset-y-0 left-2 w-px bg-zinc-900/10"
+            style={{ transform: "none", transformOrigin: "50% 50% 0px" }}
           ></div>
 
           {/* POINTER */}
           <div
             className="absolute left-2 h-6 w-px bg-emerald-500"
             data-projection-id="27"
-            // style="top: 4px; opacity: 1; transform: none; transform-origin: 50% 50% 0px;"
+            // style={{ top: "4px", opacity: 1, transform: "none", SVGTransform-origin: "50% 50% 0px"}}
           ></div>
 
           <ul role="list" className="border-l border-transparent">
